281e2afee75b4c6c07893f785cc41136a5029bfd
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2026-06-25 23:41:47 -0500

    creates a MIDI file

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from PIL import Image
-from music21 import stream, note
+from music21 import stream, note, instrument
 
 img = Image.open("SyntronicLabsStandup.png").convert("L")
 
 s = stream.Stream()
+s.append(instrument.Piano())
 
 # Convert image data to list first, then slice
 pixels = list(img.getdata())
@@ -11,4 +12,7 @@ for pixel in pixels[::500]:
   n = note.Note(60 + pixel//15)
   s.append(n)
 
-s.show('text')
+# Export to MIDI file
+s.write('midi', fp='output.mid')
+print("✓ MIDI file saved as 'output.mid'")
+print(f"✓ Generated {len(list(img.getdata())[::500])} notes from image")
